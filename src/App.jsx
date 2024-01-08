6e6aba7c18faa00dd56761c17dc8a3990f745bd5
raihannoorhasan <raihanc39@gmail.com>
2024-01-08 20:32:23 +0600

    project file init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  
+  return (
+    <>
+     <h1 className="bg-red-900 text-white text-3xl font-sans font-bold text-center p-4">Hello World!</h1>
+    </>
+  )
+}
+
+export default App
